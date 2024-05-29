f5bc08b9acf82b1679fe7ca1d3be42e961c2b6c1
Ghesse-p <ghessenonato@aluno.unifapce.edu.br>
2024-05-29 18:39:55 -0300

    tela de cadastro de clientes alterada e estilizada com inserção de submenu. tela de dashboard fora da pasta de admin excluida.

--- Front-end/admin/cadastro_clientes.js
+++ Front-end/admin/cadastro_clientes.js
@@ -1,9 +1,21 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-var formData = new FormData(this);
-fetch('cadastrar_cliente.php', {
-    method: 'POST',
-    body: formData
-}).then(response => response.text())
-  .then(data => console.log(data));
+document.addEventListener('DOMContentLoaded', function() {
+  var cadastrosGeraisLink = document.getElementById('cadastros-gerais-link');
+  var cadastrosGeraisOptions = document.getElementById('cadastros-gerais-options');
+
+  cadastrosGeraisLink.addEventListener('click', function(e) {
+      e.preventDefault();
+      cadastrosGeraisOptions.classList.toggle('hidden');
+      cadastrosGeraisLink.parentElement.classList.toggle('submenu-active');
+  });
+
+  document.querySelector('form').addEventListener('submit', function(e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      fetch('cadastrar_cliente.php', {
+          method: 'POST',
+          body: formData
+      })
+      .then(response => response.text())
+      .then(data => console.log(data));
+  });
 });
